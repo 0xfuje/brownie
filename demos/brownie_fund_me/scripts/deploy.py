450e338aad3241099f836d76010f28af399d6e3c
Bence <blockchaineddev@gmail.com>
2021-11-05 17:19:36 +0100

    working on brownie_fund_me, writing test..

--- demos/brownie_fund_me/scripts/deploy.py
+++ demos/brownie_fund_me/scripts/deploy.py
@@ -1,16 +1,21 @@
 from brownie import FundMe, MockV3Aggregator, network, config
-from scripts.helpful_scripts import get_account, deploy_mocks
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 from web3 import Web3
 
 
 def deploy_fund_me():
     account = get_account()
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
     else:
         deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
 
     fund_me = FundMe.deploy(
         price_feed_address,
@@ -18,6 +23,7 @@ def deploy_fund_me():
         publish_source=config["networks"][network.show_active()].get("verify"),
     )
     print(f"Contract deployed to {fund_me.address}")
+    return fund_me
 
 
 def main():
